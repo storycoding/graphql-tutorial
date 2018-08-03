adfc26ca9723e4a87745d1a83a391142ed701467
storycoding <youknownuno@gmail.com>
2018-08-03 16:54:49 +0100

    created dummyData to simulate a noSQL database

---
+++ dummyData/books.js
@@ -0,0 +1,8 @@
+const books = [
+	{ id: '1', title:'Mastery' },
+	{ id: '2', title:'The art of war' },
+	{ id: '3', title:'Lord of the flies' },
+	{ id: '4', title:'The holy bible' }
+]
+
+module.exports = books
