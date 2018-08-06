8ef55afc5f8cf141e92072bd4f74fb526f222fec
storycoding <youknownuno@gmail.com>
2018-08-06 11:44:36 +0100

    created authors, and entry ids to connect it with books at a database level

---
+++ dummyData/authors.js
@@ -0,0 +1,7 @@
+const authors = [
+	{ id: '1', age: 59, name:'Robert Greene' },
+	{ id: '2', age: 75, name:'Sun Tzu' },
+	{ id: '3', age: 82, name:'William Golding' }
+]
+
+module.exports = authors
--- dummyData/books.js
+++ dummyData/books.js
@@ -1,8 +1,8 @@
 const books = [
-	{ id: '1', title:'Mastery' },
-	{ id: '2', title:'The art of war' },
-	{ id: '3', title:'Lord of the flies' },
-	{ id: '4', title:'The holy bible' }
+	{ id: '1', title:'Mastery', authorId:'1' },
+	{ id: '2', title:'The art of war', authorId:'2' },
+	{ id: '3', title:'Lord of the flies', authorId:'3' },
+	{ id: '4', title: 'The 48 laws of power', authorId: '1' }
 ]
 
 module.exports = books
